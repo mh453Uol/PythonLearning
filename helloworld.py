1277a642f0a9112b0c682818c480006b69e6a814
Hussain <Majid.Hussain@lubrizol.com>
2017-07-06 14:13:40 +0100

    Basic types and function definitions

---
+++ helloworld.py
@@ -0,0 +1,53 @@
+print("Hello World!");
+print(1+1-2);
+
+# In python we dont need to specify the type
+answer = 42
+print(answer);
+
+# This function adds two type however 
+# not specifying types has its flaws
+def add_numbers(a, b):
+    print(a + b)
+
+add_numbers(10, 10)
+add_numbers("Majid is ", "life")
+#add_numbers(11,"Majid is "); doesnt work type error
+
+
+# Type Hinting helps Ide but not compiliers
+def add_numbers2(a: int,b: int) -> int:
+    print(a + b)
+
+add_numbers2(1, 2)
+
+#                   Types in python
+
+# Integer
+answer = 42
+
+# Float
+pi = 3.14159 
+
+print(answer)
+print(pi)
+
+# Float 
+print(answer + pi);
+
+#                       CASTING
+
+# Casting from float to int
+print(int(pi) == 3)
+print(int(pi))
+print(int("21")) # 21 as int
+print(float("0.123")) #0.123 as float
+
+# Different string syntax
+print('HelloWorld' == "HelloWorld" == """HelloWorld""")
+# Uppercase first letter
+print("hello".capitalize() == "Hello")
+print("foo bar".capitalize()) # Foo bar
+print("hallo".replace("a","e")) # hello
+print("hello".isalpha()) # check if 1 or more alphabetic letter
+print("123".isdigit() == True) # useful if convert to int
