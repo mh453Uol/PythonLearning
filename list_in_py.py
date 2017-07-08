3586cac6da69c4d1fecf57c233f019181d40bf82
mh453Uol <hussainmajid95@gmail.com>
2017-07-08 11:16:34 +0100

    list in py

---
+++ list_in_py.py
@@ -0,0 +1,22 @@
+# List a collection of elements
+student_names = []
+
+# You can have different types 
+# Initializing a list
+student_names = ["Mark","Katarina","Jessica",1]
+
+print(student_names)
+
+# Get index of list
+# student = student_names[1]
+# print(student)
+
+# First Element
+print(student_names[0])
+
+# Last Elements so 1
+print(student_names[-1])
+# Second Last Element so "Jessica"
+print(student_names[-2])
+# Third Last ELement so "Katarina"
+print(student_names[-3])
