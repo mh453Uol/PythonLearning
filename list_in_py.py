0ebfa93820789bf33c34116fe3ac37e4a4227358
mh453Uol <hussainmajid95@gmail.com>
2017-07-09 09:51:24 +0100

    Exceptions and dictionary

--- list_in_py.py
+++ list_in_py.py
@@ -69,3 +69,86 @@ for name in student_names:
         print("Found Jessica") # not runned because of continue
     print("Currently Iterating {0}".format(name))
 
+# While Loops
+
+x = 0
+while x < 10:
+    print("Count is {0}".format(x))
+    x += 1
+    
+# Infinite Loop
+num = 43
+while True:
+    if num == 43:
+        break
+    print(num)
+
+
+# Dictionaries key value pair
+car = {
+    "reg":"ejo33221",
+    "manufacture": "Toyota",
+    "model": "Yaris 1.3",
+    "owner":"Majid",
+    "owner_id": 1
+}
+
+print(car)
+
+# list of dictionary
+student = [
+    {"name":"Mark", "id": 123, "feedback":None },
+    {"name":"Simon", "id": 83, "feedback":"Great Class" }
+]
+
+print(student)
+
+# dictionary methods
+# get owner from dictionary where key equals Majid
+print(car["owner"] == "Majid")
+
+# if you get a key that doesnt exist you get a key error
+#print((car["foo_bar"] == KeyError))
+
+# nice method to get key value
+print(car.get("owner","Majid"))
+
+# rather than manually handling key error
+# .get returns unknown if it cant 
+# find the key value pair
+print(car.get("foo_bar","s"))
+
+# get all keys
+print(car.keys())
+# get all values
+print(car.values())
+
+# updating key value pairs
+# in list first need to find dictionary to 
+#update when specify name and value
+student[0]["name"] = "James"
+print(student)
+
+# deleting dictionary key vallue pair in list
+print("Starting deleting ----------")
+del student[0]["name"]
+print(student)
+
+# handling exceptions
+try:    
+    student[0]["wont_work"]
+except KeyError:
+    print("Cant find key property")
+
+print("This should execute")
+
+# chaining exceptions
+
+try:    
+    car["owner"]
+    0/10
+except KeyError:
+    print("Cant find key property")
+except ZeroDivisionError:
+    print("Stupid")
+
