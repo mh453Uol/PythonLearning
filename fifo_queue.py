f4e2a2ce1f878741860e2166285f291bdf86e44b
mh453Uol <hussainmajid95@gmail.com>
2017-07-12 20:00:48 +0100

    implemented a fifo queue using list

---
+++ fifo_queue.py
@@ -0,0 +1,36 @@
+# fifo queue implementation
+
+# 1,2,3,4,5,
+queue = []
+
+def get_queue():
+    return queue
+
+def push(element):
+    queue.append(element)
+
+def pop():
+    queue[0] = None
+    shift_index = 1
+
+    while shift_index < get_queue_size(queue):
+            queue[shift_index-1] = queue[shift_index]
+            shift_index += 1
+    del queue[-1]
+
+def get_queue_size(lists:[]):
+    return len(lists)
+
+push(1)
+push(2)
+print(queue)
+push(3)
+print(queue)
+push(4)
+print(queue)
+push(5)
+print(queue)
+pop()
+print(queue)
+pop()
+print(queue)
